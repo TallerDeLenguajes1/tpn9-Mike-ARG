88968f8158aa707695f8bdea27b4bdd5c10cb303
Miguel <milito_vanetta@hotmail.com>
2020-07-06 02:54:33 -0300

    TP terminado, pero falta debuggearlo bien

---
+++ TP9/TP9/Helpers.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Helpers
+{
+    public static class SoporteParaConfiguracion
+    {
+        public static void CrearArchivoDeConfiguracion(string Destino)
+        {
+            BinaryWriter Binario = new BinaryWriter(File.Open(Destino + @"\destino.dat", FileMode.OpenOrCreate));
+            Binario.Write(Destino);
+            Binario.Close();
+        }
+        public static string LeerConfiguracion(string Destino)
+        {
+            BinaryReader LeerBinario = new BinaryReader(File.Open(Destino + @"\destino.dat", FileMode.Open));
+            string contenido = LeerBinario.ReadString();
+            LeerBinario.Close();
+            return contenido;
+        }
+    }
+
+    public static class ConversorDeMorse
+    {
+
+        static Dictionary<string, string> DiccionarioMorse = new Dictionary<string, string>()
+        {
+            {"a", ".-"}, {"b", "-..."}, {"c", "-.-."}, {"d", "-.."}, {"e", "."},
+            {"f", "..-." }, {"g", "--."}, {"h", "...."}, {"i", ".."}, {"j", ".---"},
+            {"k", "-.-" }, {"l", ".-.."}, {"m", "--"}, {"n", "-."}, {"ñ", "--.--"},
+            {"o", "---"}, {"p", ".--."}, {"q", "--.-"}, {"r", "-.-"}, {"s", "..."},
+            {"t", "-"}, {"u", "..-"}, {"v", "...-"}, {"w", ".--"}, {"x", "-..-"},
+            {"y", "-.--"}, {"z","--.."}, {"1", ".----"}, {"2", "..---"}, {"3", "...--"},
+            {"4", "....-"}, {"5", "....."}, {"6", "-...."}, {"7", "--..."}, {"8", "---.."},
+            {"9", "----."}, {"0", "-----"}, {" ", "/"}
+        };
+
+
+        public static string MorseATexto(string cadena)
+        {
+            string traduccion = "";
+
+            foreach (char simbolo in cadena)
+            {
+                foreach(KeyValuePair<string, string> elemento in DiccionarioMorse)
+                {
+                    if(elemento.Value == Convert.ToString(simbolo))
+                    {
+                        traduccion = traduccion + elemento.Key;
+                        break;
+                    }
+                }
+            }
+
+            return traduccion;
+        }
+
+        public static string TextoAMorse(string cadena)
+        {
+            string traduccion = "";
+
+            cadena = cadena.ToLower();
+
+            foreach(char letra in cadena)
+            {
+                foreach(KeyValuePair<string, string> elemento in DiccionarioMorse)
+                {
+                    if(Convert.ToChar(elemento.Key) == letra)
+                    {
+                        traduccion = traduccion + elemento.Value;
+                        break;
+                    }
+                }
+            }
+
+            return traduccion;
+        }
+    }
+}
--- TP9/TP9/Program.cs
+++ TP9/TP9/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using Helpers;
 
 namespace TP9
 {
@@ -6,7 +10,52 @@ namespace TP9
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string Directorio = @"C:\Repogit\tp9\";
+
+            if (!Directory.Exists(Directorio + @"bin\debug\"))
+            {
+                Directory.CreateDirectory(Directorio + @"bin\debug\");
+            }
+
+            if (!Directory.Exists(Directorio + @"Morse\"))
+            {
+                Directory.CreateDirectory(Directorio + @"Morse\");
+            }
+
+            List<string> ListaDeArchivos = Directory.GetFiles(Directorio + @"\bin\debug\").ToList();
+
+            SoporteParaConfiguracion.CrearArchivoDeConfiguracion(Directorio + @"Morse\");
+
+            Console.WriteLine("---------- Lista de Archivos ----------");
+            Console.WriteLine("");
+
+            foreach(string archivo in ListaDeArchivos)
+            {
+                Console.WriteLine(archivo);
+
+                if(Path.GetExtension(archivo) == ".mp3" || Path.GetExtension(archivo) == ".txt")
+                {
+                    Directory.Move(archivo, Directorio + @"Morse\" + Path.GetFileName(archivo));
+                }
+            }
+
+            string ruta = Directorio + @"Morse\";
+
+            Console.WriteLine("\nIngrese una cadena de texto: ");
+            string cadena = Console.ReadLine();
+
+            string codigoMorse = ConversorDeMorse.TextoAMorse(cadena);
+            string fecha = DateTime.Now.ToString("dd-MM-yy");
+
+            string rutaAux = ruta + "morse_[" + fecha + "].txt";
+
+            File.WriteAllText(rutaAux, codigoMorse);
+
+            string codigoMorse2 = File.ReadAllText(rutaAux);
+            string textoOriginal = ConversorDeMorse.MorseATexto(codigoMorse2);
+            string rutaTexto = ruta + "morseTraducido_[" + fecha + "].txt";
+            File.WriteAllText(rutaTexto, textoOriginal);
+            Console.WriteLine("Texto original: " + textoOriginal);
         }
     }
 }
